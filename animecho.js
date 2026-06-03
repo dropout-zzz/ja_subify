879277b069f066487f79316e1a9457497508ff9e
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-06-03 23:30:32 +0900

    edit: impl basic rendering and improve webview experience

--- animecho.js
+++ animecho.js
@@ -1,7 +1,19 @@
+const KEY_ANIMECHO_SCROLL = 'animecho_scroll';
+
 const animechoRoot = document.getElementById('animecho-root');
 
-function animechoUpdateContent(s) {
+function animechoUpdateContent(s, restore_scroll) {
   animechoRoot.innerHTML = s;
+
+  if (restore_scroll) {
+    const saved_scroll = localStorage.getItem(KEY_ANIMECHO_SCROLL);
+
+    if (saved_scroll !== null)
+      scrollTo(0, parseInt(saved_scroll, 10));
+  }
+
+  // fade-in
+  document.documentElement.style.opacity = '1';
 }
 
 const animechoWebChan = new QWebChannel(qt.webChannelTransport, function(channel) {
@@ -10,4 +22,8 @@ const animechoWebChan = new QWebChannel(qt.webChannelTransport, function(channel
   animecho.nativeUpdateContent.connect(animechoUpdateContent)
 
   animecho.notifyNativeLoadFinished();
+});
+
+addEventListener('beforeunload', function(event) {
+  localStorage.setItem(KEY_ANIMECHO_SCROLL, scrollY.toString());
 });
--- animecho_rc.py
+++ animecho_rc.py
@@ -6,57 +6,93 @@
 from PySide6 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x01F\
+\x00\x00\x01{\
 <\
 !doctype html>\x0a<\
 html lang=\x22ja\x22>\x0a\
   <head>\x0a    <me\
 ta charset=\x22utf-\
-8\x22/>\x0a    <link h\
-ref=\x22qrc:///anim\
-echo/animecho.cs\
-s\x22 rel=\x22styleshe\
-et\x22/>\x0a  </head>\x0a\
-  <body>\x0a    <di\
-v id=\x22animecho-r\
-oot\x22></div>\x0a    \
-<script src=\x22qrc\
-:///qtwebchannel\
-/qwebchannel.js\x22\
-></script>\x0a    <\
-script src=\x22qrc:\
-///animecho/anim\
-echo.js\x22></scrip\
-t>\x0a  </body>\x0a</h\
-tml>\x0a\
-\x00\x00\x01q\
+8\x22/>\x0a    <meta n\
+ame=\x22color-schem\
+e\x22 content=\x22ligh\
+t dark\x22/>\x0a    <l\
+ink href=\x22qrc://\
+/animecho/animec\
+ho.css\x22 rel=\x22sty\
+lesheet\x22/>\x0a  </h\
+ead>\x0a  <body>\x0a  \
+  <div id=\x22anime\
+cho-root\x22></div>\
+\x0a    <script src\
+=\x22qrc:///qtwebch\
+annel/qwebchanne\
+l.js\x22></script>\x0a\
+    <script src=\
+\x22qrc:///animecho\
+/animecho.js\x22></\
+script>\x0a  </body\
+>\x0a</html>\x0a\
+\x00\x00\x03\x18\
 c\
+onst KEY_ANIMECH\
+O_SCROLL = 'anim\
+echo_scroll';\x0a\x0ac\
 onst animechoRoo\
 t = document.get\
 ElementById('ani\
 mecho-root');\x0a\x0af\
 unction animecho\
-UpdateContent(s)\
+UpdateContent(s,\
+ restore_scroll)\
  {\x0a  animechoRoo\
 t.innerHTML = s;\
-\x0a}\x0a\x0aconst animec\
-hoWebChan = new \
-QWebChannel(qt.w\
-ebChannelTranspo\
-rt, function(cha\
-nnel) {\x0a  const \
-animecho = chann\
-el.objects.anime\
-cho;\x0a\x0a  animecho\
-.nativeUpdateCon\
-tent.connect(ani\
-mechoUpdateConte\
-nt)\x0a\x0a  animecho.\
-notifyNativeLoad\
-Finished();\x0a});\x0a\
-\
-\x00\x00\x00\x00\
-\
+\x0a\x0a  if (restore_\
+scroll) {\x0a    co\
+nst saved_scroll\
+ = localStorage.\
+getItem(KEY_ANIM\
+ECHO_SCROLL);\x0a\x0a \
+   if (saved_scr\
+oll !== null)\x0a  \
+    scrollTo(0, \
+parseInt(saved_s\
+croll, 10));\x0a  }\
+\x0a\x0a  // fade-in\x0a \
+ document.docume\
+ntElement.style.\
+opacity = '1';\x0a}\
+\x0a\x0aconst animecho\
+WebChan = new QW\
+ebChannel(qt.web\
+ChannelTransport\
+, function(chann\
+el) {\x0a  const an\
+imecho = channel\
+.objects.animech\
+o;\x0a\x0a  animecho.n\
+ativeUpdateConte\
+nt.connect(anime\
+choUpdateContent\
+)\x0a\x0a  animecho.no\
+tifyNativeLoadFi\
+nished();\x0a});\x0a\x0aa\
+ddEventListener(\
+'beforeunload', \
+function(event) \
+{\x0a  localStorage\
+.setItem(KEY_ANI\
+MECHO_SCROLL, sc\
+rollY.toString()\
+);\x0a});\x0a\
+\x00\x00\x00j\
+b\
+ody {\x0a  font-siz\
+e: 125%;\x0a}\x0a\x0ahtml\
+ {\x0a  /* for fade\
+-in */\x0a  transit\
+ion: opacity 800\
+ms ease;\x0a  opaci\
+ty: 0;\x0a}\x0a\
 "
 
 qt_resource_name = b"\
@@ -83,12 +119,12 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00R\x00\x00\x00\x00\x00\x01\x00\x00\x02\xbf\
-\x00\x00\x01\x9e\x8a\x91\xec\xe8\
-\x00\x00\x006\x00\x00\x00\x00\x00\x01\x00\x00\x01J\
-\x00\x00\x01\x9e\x8az\x16\xe2\
+\x00\x00\x00R\x00\x00\x00\x00\x00\x01\x00\x00\x04\x9b\
+\x00\x00\x01\x9e\x8d\xd1n\x91\
+\x00\x00\x006\x00\x00\x00\x00\x00\x01\x00\x00\x01\x7f\
+\x00\x00\x01\x9e\x8d\xd0.\xf4\
 \x00\x00\x00\x16\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x9e\x8a\x96\xb6S\
+\x00\x00\x01\x9e\x8dv<k\
 "
 
 def qInitResources():
