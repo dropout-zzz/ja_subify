0b39a1cf5456f7be9081c2628d632dcda2450f86
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-07-21 14:57:41 +0900

    extend loan word db format
    closes #6

--- loan_word_db.py
+++ loan_word_db.py
@@ -7,11 +7,47 @@
 from dataclasses import dataclass
 import dataclasses
 
+COL_SEP = '\t'
+FRAG_SEP_A = '\x00'
+FRAG_SEP_B = ' '
+
+@dataclass
+class LoanFragment:
+  """smallest part in loan word that can be
+     correspond to its form in english."""
+  base: str
+  romanized: str
+
 @dataclass
 class LoanWord:
   """a word entry in the dictionary."""
   base: str
-  romanized: str
+  fragments: list[LoanFragment] = dataclasses.field(default_factory=list)
+
+  def get_normalized(self) -> str:
+    """return romanized word."""
+    return ' '.join(x.romanized for x in self.fragments)
+
+  def serialize(self) -> str:
+    a = []
+    b = []
+
+    for x in self.fragments:
+      a.append(x.base)
+      b.append(x.romanized)
+
+    return f'{FRAG_SEP_A.join(a)}{COL_SEP}{FRAG_SEP_B.join(b)}'
+
+  @classmethod
+  def deserialize(cls, s: str):
+    a, b = s.split(COL_SEP)
+    base_parts = a.split(FRAG_SEP_A)
+
+    fragments = []
+    for base, romanized in zip(base_parts, b.split(FRAG_SEP_B), strict=True):
+      fragments.append(LoanFragment(base=base, romanized=romanized))
+
+    return cls(fragments=fragments, base=''.join(base_parts))
 
 @dataclass
 class LoanWordList:
@@ -23,17 +59,27 @@ class LoanWordList:
     to get the list of entries in the
     file, use inner.values()
 
+     `known' holds a mapping for known set of loan word fragments.
+     this field is only populated when `count_known' is True.
+
+     the flag should be specified when creating the object or
+     when using the load_loan_words() function.
+
     use append_into_loan_words(),
     instead of directly modifying the internal dict."""
 
   inner: dict[str, LoanWord] = dataclasses.field(default_factory=dict)
+  known: set[tuple[str, str]] = dataclasses.field(default_factory=set)
   off: int = 0
-
-COL_SEP = '\t'
+  count_known: bool = False
 
 class LoanWordAlreadyExist(Exception):
   """thrown when trying to append a word that already exist"""
   pass
+
+def _register_known_loan_words(lwl: LoanWordList, word: LoanWord):
+  for fragment in word.fragments:
+    lwl.known.add((fragment.base, fragment.romanized))
 
 def append_into_loan_words(lwl: LoanWordList, path: str, word: LoanWord):
   """add a word into the word list object.
@@ -43,21 +89,33 @@ def append_into_loan_words(lwl: LoanWordList, path: str, word: LoanWord):
      may throw LoanWordAlreadyExist"""
   if word.base in lwl.inner:
     raise LoanWordAlreadyExist
+
   with open(path, 'ab') as f:
-    lwl.off += f.write(f'{word.base}{COL_SEP}{word.romanized}\n'.encode())
+    n = f.write(word.serialize().encode())
+    f.write(b'\n')
+    lwl.off += n + 1
+
   lwl.inner[word.base] = word
+
+  if lwl.count_known:
+    _register_known_loan_words(lwl, word)
 
 def _load_into_loan_words(lwl: LoanWordList, s: str):
   for line in s.splitlines():
-    base, romanized = line.split(COL_SEP)
-    if base in lwl.inner:
-      raise ValueError(f'duplicate word {base!r}')
-    lwl.inner[base] = LoanWord(base=base, romanized=romanized)
+    word = LoanWord.deserialize(line)
 
-def load_loan_words(path: str) -> LoanWordList:
+    if word.base in lwl.inner:
+      raise ValueError(f'duplicate word {word.base!r}')
+
+    lwl.inner[word.base] = word
+
+    if lwl.count_known:
+      _register_known_loan_words(lwl, word)
+
+def load_loan_words(path: str, count_known: bool = False) -> LoanWordList:
   """read and parse a loan word list file from disk."""
   with open(path, 'rb') as f:
-    lwl = LoanWordList()
+    lwl = LoanWordList(count_known=count_known)
 
     _load_into_loan_words(lwl, (content := f.read()).decode())
     lwl.off = len(content)
