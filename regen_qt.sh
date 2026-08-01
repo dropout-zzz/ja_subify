d173d68cc8c024aac8965e19af05c26991a904c0
Zz <267457761+dropout-zzz@users.noreply.github.com>
2026-08-02 02:46:03 +0900

    {edit,viewer}: replace dflt win icon with a new

--- regen_qt.sh
+++ regen_qt.sh
@@ -7,3 +7,5 @@ set -e
 # this script assume you're on archlinux.
 
 /usr/lib/qt6/rcc --no-compress -g python animecho.qrc -o animecho_rc.py
+
+/usr/lib/qt6/rcc -g python eye_candies.qrc -o eye_candies_rc.py
